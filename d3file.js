275b73b218061b0668cd14c6fbed11be83f0c240
sidjayisaok <sidjay18@gmail.com>
2016-09-26 13:45:00 -0400

    added ES6 syntax and some functions

--- d3file.js
+++ d3file.js
@@ -1,8 +1,8 @@
 //json to define rectangle and circles
-var jsonRectangle =[
+let jsonRectangle =[
   {
     "x_axis": 50,
-    "y_axis": 175,
+    "y_axis": 275,
     "width": 100,
     "height": 100,
     "color": "blue"
@@ -10,7 +10,7 @@ var jsonRectangle =[
 
   {
     "x_axis": 250,
-    "y_axis": 175,
+    "y_axis": 275,
     "width": 125,
     "height": 125,
     "color": "yellow"
@@ -18,127 +18,138 @@ var jsonRectangle =[
 
   {
     "x_axis": 500,
-    "y_axis": 175,
+    "y_axis": 275,
     "width": 150,
     "height": 150,
     "color": "orange"
   }
 ];
 
-var jsonCircles =[
+let jsonCircles =[
   {
     "x_axis": 30,
-    "y_axis": 75,
+    "y_axis": 150,
     "radius": 20,
     "color": "green"
   },
 
   {
     "x_axis": 300,
-    "y_axis": 75,
-    "radius": 20,
+    "y_axis": 150,
+    "radius": 40,
     "color": "purple"
   },
 
   {
     "x_axis": 570,
-    "y_axis": 75,
-    "radius": 20,
+    "y_axis": 150,
+    "radius": 80,
     "color": "red"
   }
 ];
 
-var barData = [1, 2, 4, 8, 16, 32, 64, 128];
+let barData = [20, 25, 30, 50, 80, 130, 210, 340];
 
 //definitions for the svgs
-var svgContain = d3.select("body").append("svg")
+let svgContain = d3.select("body").append("svg")
               .attr("width", 1300)
               .attr("height", 1300);
 //draws a circle
-var circles = svgContain.selectAll("circle")
+let circles = svgContain.selectAll("circle")
             .classed("myCircle", true)
             .data(jsonCircles)
             .enter()
             .append("circle");
 //draws a rectangle
-var rectangle = svgContain.selectAll("rect")
+let rectangle = svgContain.selectAll("rect")
               .classed("myRectangle", true)
               .data(jsonRectangle)
               .enter()
               .append("rect");
 
 //works with chart below
-var x = d3.scale.linear()
+let x = d3.scale.linear()
         .domain([0, d3.max(barData)])
         .range([0, 420]);
 
 //attempting to draw bar chart
-var barChart =  d3.select("div")
+let barChart =  d3.select("div")
                 .selectAll(".chart")
                 .data(barData)
                 .enter()
                 .append("div")
-                .style("width", function(d){
+                .style("width", (d)=>{
                   return x(d) + "px";
                 })
-                .style("background-color", "teal")
+                .style("background-color", (d)=>{
+                  if (d < 30) {
+                    return "red";
+                  }
+                  else if (d > 20 && d < 81){
+                    return "orange";
+                  }
+                  else{
+                    return "teal";
+                  }
+                })
+                .style("color", "white")
                 .attr("class", "myChart")
-                .text(function(d){
+                .text((d)=>{
                   return d;
                 });
 
 //circle properties
-var circleAttributes = circles
-                    .attr("cx", function(d){
+let circleAttributes = circles
+                    .attr("cx", (d)=>{
                       return d.x_axis;
                     })
-                    .attr("cy", function(d){
+                    .attr("cy", (d)=>{
                       return d.y_axis;
                     })
-                    .attr("r", function(d){
+                    .attr("r", (d)=>{
                       return d.radius;
                     })
-                    .style("fill", function(d) {
+                    .style("fill", (d)=> {
                       return d.color;
                    });
 
 //rectangle properties
-var rectAttributes = rectangle
-                    .attr("x", function(d){
+let rectAttributes = rectangle
+                    .attr("x", (d)=>{
                       return d.x_axis;
                     })
-                    .attr("y", function(d){
+                    .attr("y", (d)=>{
                       return d.y_axis;
                     })
-                    .attr("width", function(d){
+                    .attr("width", (d)=>{
                       return d.width;
                     })
-                    .attr("height", function(d){
+                    .attr("height", (d)=>{
                       return d.height;
                     })
-                    .style("fill", function(d){
+                    .style("fill", (d)=>{
                       return d.color;
                     });
 
 //animate.css jquery controls
 $(function(){
 //variables added to reduce repetition
-var bounceClass ='animated jello';
-var rubberClass = 'animated rubberBand';
-var tadaClass = 'animated tada';
-var flipClass ='animated shake';
-var fadeinClass = 'animated fadeInLeft';
-var fadeoutClass = 'animated fadeOutLeft';
-var endClass ='webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+let bounceClass ='animated jello';
+let rubberClass = 'animated rubberBand';
+let tadaClass = 'animated tada';
+let flipClass ='animated shake';
+let fadeinClass = 'animated fadeInLeft';
+let fadeoutClass = 'animated fadeOutLeft';
+let endClass ='webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 //animates rectangles
 $('rect').on({
- 'click': function(){
+ 'click': ()=>{
      $('rect').addClass(bounceClass).one(endClass, function(){
          $(this).removeClass(bounceClass);
      })
  },
 //this makes them animate when hovering over them via the mouse
- 'mouseover': function(){
+ 'mouseover': ()=>{
      $('rect').addClass(flipClass).one(endClass, function(){
          $(this).removeClass(flipClass);
      })
@@ -147,13 +158,13 @@ $('rect').on({
 });
 //animates circles
 $('circle').on({
- 'click': function(){
+ 'click': ()=>{
      $('circle').addClass(rubberClass).one(endClass, function(){
          $(this).removeClass(rubberClass);
      })
  },
 //this makes them animate when hovering over them via the mouse
- 'mouseover': function(){
+ 'mouseover': ()=>{
      $('circle').addClass(tadaClass).one(endClass, function(){
          $(this).removeClass(tadaClass);
      })
@@ -163,13 +174,13 @@ $('circle').on({
 
 //animates bar chart
 $('.myChart').on({
- 'click': function(){
+ 'click': ()=>{
      $('.myChart').addClass(fadeinClass).one(endClass, function(){
          $(this).removeClass(fadeinClass);
      })
  },
 //this makes them animate when hovering over them via the mouse
- 'mouseover': function(){
+ 'mouseover': ()=>{
      $('.myChart').addClass(fadeoutClass).one(endClass, function(){
          $(this).removeClass(fadeoutClass);
      })
